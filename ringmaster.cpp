fd939cac4e1522e040511c6cff410b5e96f8df55
VCM admin account <vcm@vcm-5999.vm.duke.edu>
2019-02-15 16:28:54 -0500

    accept

---
+++ ringmaster.cpp
@@ -0,0 +1,62 @@
+#include "check.h"
+
+using namespace std;
+
+vector<Player> players;
+
+int main(int argc, char *argv[]) {
+  int status;
+  int socket_fd;
+  int player_num = 3;
+  struct addrinfo host_info;
+  struct addrinfo *host_info_list;
+  const char *hostname = NULL;
+  const char *port = "23333";
+
+  memset(&host_info, 0, sizeof(host_info));
+
+  host_info.ai_family = AF_UNSPEC;
+  host_info.ai_socktype = SOCK_STREAM;
+  host_info.ai_flags = AI_PASSIVE;
+
+  status = getaddrinfo(hostname, port, &host_info, &host_info_list);
+  checkHostInfo(status, hostname, port);
+
+  socket_fd = socket(host_info_list->ai_family, host_info_list->ai_socktype,
+                     host_info_list->ai_protocol);
+  checkCreateServerSocket(socket_fd, hostname, port);
+
+  int yes = 1;
+  status = setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+  status = bind(socket_fd, host_info_list->ai_addr, host_info_list->ai_addrlen);
+  checkIfBind(status, hostname, port);
+
+  status = listen(socket_fd, 100);
+  checkIfListen(status, hostname, port);
+
+  for (int i = 0; i < player_num; i++) {
+    cout << "Waiting for player connection on port" << port << endl;
+    struct sockaddr_storage socket_addr;
+    socklen_t socket_addr_len = sizeof(socket_addr);
+    int client_fd;
+    client_fd =
+        accept(socket_fd, (struct sockaddr *)&socket_addr, &socket_addr_len);
+    checkCreateClientSocket(client_fd);
+
+    string client_ip = getClientIP(socket_addr);
+    int client_port = getClientPort(socket_addr);
+
+    int player_seq = createPlayer(players, client_fd, client_ip, client_port);
+    cout << "Seq: " << player_seq << " FD: " << client_fd
+         << " IP: " << client_ip << " Port: " << client_port << endl;
+    char buffer[512];
+    recv(client_fd, buffer, 512, 0);
+    buffer[512] = 0;
+
+    cout << "Server received: " << buffer << endl;
+  }
+
+  freeaddrinfo(host_info_list);
+  close(socket_fd);
+  return 0;
+}
