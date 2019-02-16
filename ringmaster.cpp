531d65cc9c3844c188a21fe4d49f54da5c4034df
WendingLin <lycoris000@126.com>
2019-02-15 19:55:20 -0500

    add select

--- ringmaster.cpp
+++ ringmaster.cpp
@@ -2,12 +2,14 @@
 
 using namespace std;
 
+#define BUF_SIZE 512
+
 vector<Player> players;
 
 int main(int argc, char *argv[]) {
   int status;
   int socket_fd;
-  fd_set read_fds;
+  fd_set rfds;
   int ret;
   int player_num = 2;
   struct addrinfo host_info;
@@ -36,6 +38,8 @@ int main(int argc, char *argv[]) {
   status = listen(socket_fd, 100);
   checkIfListen(status, hostname, port);
 
+  cout << "-------------CREATE PLAYERS----------------" << endl;
+  /* CREATE PLAYER & SEND INFO TO PLAYER */
   for (int i = 0; i < player_num; i++) {
     cout << "Waiting for player connection on port" << port << endl;
     struct sockaddr_storage socket_addr;
@@ -47,25 +51,50 @@ int main(int argc, char *argv[]) {
 
     string client_ip = getClientIP(socket_addr);
     int client_port = getClientPort(socket_addr);
-
     int player_seq = createPlayer(players, client_fd, client_ip, client_port);
     cout << "Seq: " << player_seq << " FD: " << client_fd
          << " IP: " << client_ip << " Port: " << client_port << endl;
-    char buffer[512];
+    char buffer[BUF_SIZE];
+    memset(buffer, 0, BUF_SIZE);
     recv(client_fd, buffer, 512, 0);
     buffer[512] = 0;
+    string
 
-    cout << "Server received: " << buffer << endl;
+            cout
+        << "Server received: " << buffer << endl;
   }
 
   while (1) {
-    int max_fd = 0;
-    FD_ZERO(&read_fds);
-    FD_SET(socket_fd, &read_fds);
-    int client_fd = players[0].client_fd;
-    const char *message = "Start Game!";
-    send(client_fd, message, strlen(message), 0);
-    sleep(3);
+    FD_ZERO(&rfds);
+    for (size_t i = 0; i < players.size(); i++)
+      FD_SET(players[i].client_fd, &rfds);
+
+    ret = select(player_num + 4, &rfds, NULL, NULL, NULL);
+    // cout << "BEFORE FOR IN ISSET" << endl;
+    checkIfSelect(ret);
+    int client_fd = -1;
+
+    for (size_t i = 0; i < players.size(); i++) {
+      if (FD_ISSET(players[i].client_fd, &rfds)) {
+        cout << "FD_ISSET" << endl;
+        client_fd = players[i].client_fd;
+        break;
+      }
+    }
+    // cout << "END FOR IN ISSET" << endl;
+    if (client_fd == -1)
+      continue;
+    else {
+      char buf[BUF_SIZE];
+      memset(buf, 0, BUF_SIZE);
+      int status = recv(client_fd, buf, BUF_SIZE, 0);
+      checkReceive(status);
+      cout << "Receive Data from :" << client_fd << endl;
+      cout << "Receive: " << buf << endl;
+    }
+    // const char *message = "Start Game!";
+    // send(client_fd, message, strlen(message), 0);
+    // sleep(3);
   }
 
   freeaddrinfo(host_info_list);
