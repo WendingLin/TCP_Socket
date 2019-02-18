33c9f74fe7b0cf709f1eff7fadd5b16eb7f8e130
WendingLin <lycoris000@126.com>
2019-02-18 13:58:22 -0500

    debug version complete

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-for ((i=0; i<3; i++))                                                                             
+for ((i=0; i<20; i++))                                                                             
 do                                                                                                            
 ./player vcm-5999.vm.duke.edu &                                                                                     
 done
