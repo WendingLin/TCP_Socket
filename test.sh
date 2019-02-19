5f8cd2a2d3d421c3e13eed082f05b2e90d71f772
WendingLin <lycoris000@126.com>
2019-02-18 20:53:27 -0500

    Final

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-for ((i=0; i<4; i++))                                                                             
+for ((i=0; i<20; i++))                                                                             
 do                                                                                                            
 ./player vcm-5999.vm.duke.edu 23333 &                                                                                     
 done
